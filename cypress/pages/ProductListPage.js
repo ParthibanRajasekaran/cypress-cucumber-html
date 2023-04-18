70cdedf1c68321ba03ed01dd2f60235247b18784
Parthiban Rajasekaran <parthibanrajasekaran@GBMB10258.local>
2023-04-18 21:13:58 +0100

    feat: adding page class for the task

--- cypress/pages/ProductListPage.js
+++ cypress/pages/ProductListPage.js
@@ -1,31 +1,51 @@
-class ProductDisplayPage {
-    getCategoryPage() {
-        return cy.get("selector for the category page");
+class ProductListPage {
+    getBreadCrumbs() {
+        return cy.get("nav[data-testid='breadcrumbsTestId']");
+    }
+
+    getFilterSection() {
+        return cy.get("div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-md-3>ul");
+    }
+
+    getProductListSection() {
+        return cy.get("div.MuiGrid-root.MuiGrid-container.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-md-9")
     }
 
     getRowsPerPageDropdown() {
-        return cy.get("selector for the rows per page dropdown");
+        return cy.get('div[data-testid="paginationRowsPerPageMenu"] div').eq(0)
+    }
+
+    getRowsPerPageDropdownList() {
+        return cy.get("ul.MuiList-root.MuiMenu-list.MuiList-padding li");
     }
 
     getShowOutOfStockCheckbox() {
-        return cy.get("selector for the show out of stock checkbox");
+        return cy.get("input[data-testid='filterCheckbox-Show Out of Stock']");
     }
 
     getSortByDropdown() {
-        return cy.get("selector for the sort by dropdown");
+        return cy.get("div[data-testid='menuSortBy']");
+    }
+
+    getSortByDropdownList() {
+        return cy.get("ul[aria-labelledby='sort-label'] li");
     }
 
     getProductList() {
-        return cy.get("div.MuiGrid-root.MuiGrid-container.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-md-9 > div[data-testid='ProductCard']");
+        return cy.get("div > div[data-testid='ProductCard']");
     }
 
     getOutOfStockProducts() {
-        return cy.get("selector for out of stock products");
+        return cy.get("div[data-testid='Availability'] > h5[data-testid='stockMessage']").filter(':contains("OUT OF STOCK")');
     }
 
-    filtersSet(){
-     return cy.get("p[data-testid='clearFiltersButton']")
+    filtersSet() {
+        return cy.get("p[data-testid='clearFiltersButton']");
+    }
+
+    productPrice() {
+        return cy.get("p[data-testid='priceLabel']");
     }
 }
 
-module.exports = new ProductDisplayPage();
+module.exports = new ProductListPage();
