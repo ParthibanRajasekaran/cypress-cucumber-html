a20ad0c97c5d11e51e7396235d2af3db3e148c61
Parthiban Rajasekaran <parthibanrajasekaran@GBMB10258.local>
2023-04-18 21:13:13 +0100

    feat: adding utility to ignore xhr requests from logs

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -21,3 +21,14 @@ import '@shelex/cypress-allure-plugin';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+// Hide fetch/XHR requests
+const app = window.top;
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+    const style = app.document.createElement('style');
+    style.innerHTML =
+        '.command-name-request, .command-name-xhr { display: none }';
+    style.setAttribute('data-hide-command-log-request', '');
+
+    app.document.head.appendChild(style);
+}
